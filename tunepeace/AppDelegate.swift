c4c511d41f126da3556e7f794d95b7f5f7e6318f
Johan Halin <johan@halin.me>
2015-10-02 13:47:50 +0300

    Initial Commit

---
+++ tunepeace/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  tunepeace
+//
+//  Created by Johan Halin on 2.10.2015.
+//  Copyright © 2015 Aero Deko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
