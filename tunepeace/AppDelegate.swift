e004f0091d16bde31b7b83da5664ed7a1a12d60a
Johan Halin <johan@halin.me>
2015-10-02 14:24:16 +0300

    Added the amazing basic UI, i.e. a menubar item that does one thing: quits the app.

--- tunepeace/AppDelegate.swift
+++ tunepeace/AppDelegate.swift
@@ -10,18 +10,25 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
 	@IBOutlet weak var window: NSWindow!
-
-
+	@IBOutlet weak var menu: NSMenu!
+	
+	var statusItem: NSStatusItem!
+	
+	@IBAction func quit(sender: AnyObject) {
+		NSApp.terminate(self)
+	}
+	
 	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
 	}
 
+	override func awakeFromNib() {		
+		self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+		self.statusItem.menu = self.menu
+		self.statusItem.button!.image = NSImage(named: "menubar")
+	}
+	
 	func applicationWillTerminate(aNotification: NSNotification) {
 		// Insert code here to tear down your application
 	}
-
-
 }
-
